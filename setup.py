e6652b9886485030011a550abe5dfe50f44481a1
mkerrin <mkerrin@62d5b8a3-27da-0310-9561-8e5933582275>
2007-07-07 20:01:53 +0000

    Bump up the version.
    git-svn-id: svn://svn.zope.org/repos/main/z3c.dav/trunk@77583 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name = "z3c.dav",
-      version = "0.9.1",
+      version = "1.0b",
       author = "Michael Kerrin",
       author_email = "michael.kerrin@openapp.ie",
       url = "http://launchpad.net/z3c.dav",
       description = "Implementation of the WebDAV protocol for Zope3",
-      long_description = open("src/z3c/dav/README.txt").read(),
+      long_description = (
+          open("src/z3c/dav/README.txt").read() +
+          "\n\n" +
+          open("CHANGES.txt").read()),
       license = "ZPL",
 
       packages = find_packages("src"),

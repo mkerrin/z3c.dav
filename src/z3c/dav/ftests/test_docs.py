77017d6ede2b1f9978e6eefe33c591e2e1cb3637
mkerrin <mkerrin@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-11 19:55:57 +0000

    Add tests to make sure that the correct properties are defined on a folder.
    git-svn-id: svn://svn.zope.org/repos/main/z3c.dav/trunk@83751 62d5b8a3-27da-0310-9561-8e5933582275

---
+++ src/z3c/dav/ftests/test_docs.py
@@ -0,0 +1,37 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os.path
+import unittest
+from zope.testing import doctest
+
+import z3c.etree.testing
+import z3c.dav.testing
+
+here = os.path.dirname(os.path.realpath(z3c.dav.testing.__file__))
+ZopeFolderDAVLayer = z3c.dav.testing.WebDAVLayerClass(
+    os.path.join(here, "ftesting.zcml"), __name__, "ZopeFolderDAVLayer")
+
+def test_suite():
+    properties = doctest.DocFileSuite(
+        "zopefolder.txt",
+        setUp = z3c.dav.testing.functionalSetUp,
+        tearDown = z3c.dav.testing.functionalTearDown,
+        checker = z3c.etree.testing.xmlOutputChecker,
+        )
+    properties.layer = ZopeFolderDAVLayer
+
+    return unittest.TestSuite((
+        properties,
+        ))
